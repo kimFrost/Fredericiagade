23601ce3091a1a0ef52e6efda5e2ed0e470215e4
Kim Frost <kim@thatworks.dk>
2017-07-07 20:19:47 +0200

    First commit

---
+++ Source/Fredericiagade/FredericiagadeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FredericiagadeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FREDERICIAGADE_API AFredericiagadeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
